3efb45cee124844eeee9fb46f46cfee44deb4b03
Parth  Kachchhi <themoonraker13@gmail.com>
2019-02-16 16:34:48 -0500

    Tokenize; Pad; Embeddings

--- classifier.py
+++ classifier.py
@@ -1,9 +1,13 @@
 import collections
 
+import h5py
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from IPython.core.display import display
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 from tqdm import tqdm
 
 # Class labels
@@ -51,3 +55,66 @@ print('{} words.'.format(len([word for sentence in x_train for word in sentence.
 print('{} unique words.'.format(len(word_counter)))
 print('10 Most common words in the dataset:')
 print('"' + '" "'.join(list(zip(*word_counter.most_common(10)))[0]) + '"')
+
+# Create tokenizer
+tokenizer = Tokenizer(num_words=None,
+                      filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+                      lower=True,
+                      split=" ",
+                      char_level=False)
+
+# Fit and run tokenizer
+tokenizer.fit_on_texts(list(x_train))
+tokenized_train = tokenizer.texts_to_sequences(x_train)
+tokenized_test = tokenizer.texts_to_sequences(x_test)
+word_index = tokenizer.word_index
+
+# Extract variables
+vocab_size = len(word_index)
+print('Vocab size: {}'.format(vocab_size))
+longest = max(len(seq) for seq in tokenized_train)
+print("Longest comment size: {}".format(longest))
+average = np.mean([len(seq) for seq in tokenized_train])
+print("Average comment size: {}".format(average))
+stdev = np.std([len(seq) for seq in tokenized_train])
+print("Stdev of comment size: {}".format(stdev))
+max_len = int(average + stdev * 3)
+print('Max comment size: {}'.format(max_len))
+print()
+
+# Pad sequences
+processed_X_train = pad_sequences(tokenized_train, maxlen=max_len, padding='post', truncating='post')
+processed_X_test = pad_sequences(tokenized_test, maxlen=max_len, padding='post', truncating='post')
+
+# Sample tokenization
+for sample_i, (sent, token_sent) in enumerate(zip(x_train[:2], tokenized_train[:2])):
+    print('Sequence {}'.format(sample_i + 1))
+    print('  Input:  {}'.format(sent))
+    print('  Output: {}'.format(token_sent))
+
+# Set embeddings dimension
+embedding_dim = 300
+
+# Get embeddings
+embeddings_index = {}
+f = open('data/wiki.en.vec', encoding="utf8")
+for line in f:
+    values = line.rstrip().rsplit(' ', embedding_dim)
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found {} word vectors.'.format(len(embeddings_index)))
+
+# Build embedding matrix
+embedding_matrix = np.zeros((len(word_index) + 1, embedding_dim))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # Words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+# Save embeddings matrix to disk
+with h5py.File('data/embeddings.h5', 'w') as hf:
+    hf.create_dataset("fasttext", data=embedding_matrix)
